40cf33843f74ffaad6c1086dc05382c9b6c5f083
Eilith404 <minaya.sofia@gmail.com>
2026-02-26 12:17:40 +0100

    Implement setup for math test and calculate person's IMC

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,23 +1,33 @@
 const { sum, multiply, divide, percentage } = require("./02-math");
 
-test("adds 1 + 3 should be 4", () => {
-  const rta = sum(1, 3);
-  expect(rta).toBe(4);
-});
+describe("Test for math functions", () => {
+  describe("Test for sum", () => {
+    test("adds 1 + 3 should be 4", () => {
+      const rta = sum(1, 3);
+      expect(rta).toBe(4);
+    });
+  });
 
-test("should be 4", () => {
-  const rta = multiply(1, 4);
-  expect(rta).toBe(4);
-});
+  describe("Test for multiply", () => {
+    test("should be 4", () => {
+      const rta = multiply(1, 4);
+      expect(rta).toBe(4);
+    });
+  });
 
-test("should divide for zero", () => {
-  const rta = divide(6, 0);
-  expect(rta).toBeNull();
-  const rta2 = divide(5, 0);
-  expect(rta2).toBeNull();
-});
+  describe("Test for divide", () => {
+    test("should divide for zero", () => {
+      const rta = divide(6, 0);
+      expect(rta).toBeNull();
+      const rta2 = divide(5, 0);
+      expect(rta2).toBeNull();
+    });
+  });
 
-test("should calculate percentage", () => {
-  const rta = percentage(20, 80);
-  expect(rta).toBe(16);
+  describe("Test for percentage", () => {
+    test("should calculate percentage", () => {
+      const rta = percentage(20, 80);
+      expect(rta).toBe(16);
+    });
+  });
 });
