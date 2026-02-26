a220a43d9bbca49b5264023180771eb3f839b2a1
Eilith404 <minaya.sofia@gmail.com>
2026-02-26 15:01:28 +0100

    Setup API enviroment with Docker and MongoDB

---
+++ api/src/app.js
@@ -0,0 +1,20 @@
+/* eslint-disable quotes */
+const express = require("express");
+const cors = require("cors");
+
+const routerApi = require("./routes");
+
+const createApp = () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  routerApi(app);
+  return app;
+};
+
+module.exports = createApp;
--- demos/src/05-person.test.js
+++ demos/src/05-person.test.js
@@ -1,13 +1,20 @@
 const Person = require("./05-person");
 
 describe("Test for Person", () => {
+  // AAA
   let person;
+  // Arrange / Given
   beforeEach(() => {
     person = new Person("Nicolas", 45, 1.7);
   });
   test("should return down", () => {
+    // Arrange / Given
     person.weight = 45;
+
+    // Act / When
     const imc = person.calcIMC();
+
+    // Assert / Then
     expect(imc).toBe("down");
   });
 
