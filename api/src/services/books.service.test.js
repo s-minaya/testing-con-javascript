b8f77b22f50303603a99429b5bf697c631eaf667
Eilith404 <minaya.sofia@gmail.com>
2026-02-27 19:28:24 +0100

    Add spy assertions to BooksService unit tests

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,3 +1,6 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable implicit-arrow-linebreak */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable quotes */
 const BooksService = require("./books.service");
@@ -9,12 +12,19 @@ const fakeBooks = [
   },
 ];
 
+const mockGetAll = jest.fn();
+
 const MongoLibStub = {
-  getAll: () => [...fakeBooks],
+  getAll: mockGetAll,
   create: () => {},
 };
 
-jest.mock("../lib/mongo.lib", () => jest.fn().mockImplementation(() => MongoLibStub));
+jest.mock("../lib/mongo.lib", () =>
+  jest.fn().mockImplementation(() => ({
+    getAll: mockGetAll,
+    create: () => {},
+  })),
+);
 
 describe("Test for BooksService", () => {
   let service;
@@ -26,23 +36,33 @@ describe("Test for BooksService", () => {
   describe("Test for getBooks", () => {
     test("Should return a list book", async () => {
       // Arrange
+      mockGetAll.mockResolvedValue(fakeBooks);
       // Act
       const books = await service.getBooks({});
       console.log(books);
 
       // Assert
       expect(books.length).toEqual(1);
+      expect(mockGetAll).toHaveBeenCalled(); // Verificar que se llamó a getAll
+      expect(mockGetAll).toHaveBeenCalledTimes(1); // Verificar que se llamó una vez
+      expect(mockGetAll).toHaveBeenCalledWith("books", {}); // Verificar que se llamó con los argumentos correctos
     });
   });
   describe("Test for getBooks", () => {
     test("Should return a list book", async () => {
       // Arrange
+      mockGetAll.mockResolvedValue([
+        {
+          _id: 1,
+          name: "Harry Potter 2",
+        },
+      ]);
       // Act
       const books = await service.getBooks({});
       console.log(books);
 
       // Assert
-      expect(books[0].name).toEqual("Harry Potter");
+      expect(books[0].name).toEqual("Harry Potter 2");
     });
   });
 });
