011478821c28d88d6639045c475381e1a6cb7711
Eilith404 <minaya.sofia@gmail.com>
2026-02-27 21:15:43 +0100

    test(e2e): add hello endpoint end-to-end test with supertest

---
+++ api/e2e/hello.e2e.js
@@ -0,0 +1,26 @@
+/* eslint-disable implicit-arrow-linebreak */
+const request = require("supertest");
+
+const createApp = require("../src/app");
+
+describe("Test for hello endpoint", () => {
+  let app = null;
+  let server = null;
+  beforeAll(() => {
+    app = createApp();
+    server = app.listen(3001);
+  });
+  afterAll(async () => {
+    await server.close();
+  });
+
+  describe("test for [GET] /", () => {
+    test("should return a string with the message 'Hello World'", () =>
+      request(app)
+        .get("/")
+        .expect(200)
+        .then((response) => {
+          expect(response.text).toEqual("Hello World!");
+        }));
+  });
+});
