f51a19302c50d8fe76f160685224c7286160f161
Eilith404 <minaya.sofia@gmail.com>
2026-03-02 20:52:23 +0100

    Fix Run e2e tests

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -4,8 +4,8 @@ const config = {
   env: process.env.NODE_ENV || "dev",
   isProd: process.env.NODE_ENV === "production",
   port: process.env.PORT || 3000,
-  dbUrl: process.env.MONGO_URL,
-  dbName: process.env.MONGO_DB_NAME,
+  dbUrl: process.env.MONGO_URL || "mongodb://test:test123@mongo-e2e:27017/demo?authSource=admin&retryWrites=true&writeConcern=majority",
+  dbName: process.env.MONGO_DB_NAME || "demo",
 };
 
 module.exports = { config };
