520cf15842ab4f9b44e198618bedde2366c8377a
Eilith404 <minaya.sofia@gmail.com>
2026-03-02 13:54:02 +0100

    Add end-to-end test with real MongoDB and seed data

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -2,42 +2,54 @@
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-const mockGetAll = jest.fn();
 
 const request = require("supertest");
 
+const { MongoClient } = require("mongodb");
+
+const { config } = require("../src/config");
+
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
+
 const createApp = require("../src/app");
-
-const { generateManyBooks } = require("../src/fakes/book.fake");
-
-const MongoLibStub = {
-  getAll: mockGetAll,
-  create: () => {},
-};
-
-jest.mock("../src/lib/mongo.lib", () =>
-  jest.fn().mockImplementation(() => ({
-    getAll: mockGetAll,
-    create: () => {},
-  })),
-);
 
 describe("Test for books", () => {
   let app = null;
   let server = null;
-  beforeAll(() => {
+  let database = null;
+  beforeAll(async () => {
     app = createApp();
     server = app.listen(3001);
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
   });
   afterAll(async () => {
     await server.close();
+    await database.dropDatabase();
   });
 
   describe("test for [GET] /api/v1/books", () => {
-    test("should return a list books", () => {
+    test("should return a list books", async () => {
       // Arrange
-      const fakeBooks = generateManyBooks(3);
-      mockGetAll.mockResolvedValue(fakeBooks);
+      const seedData = await database.collection("books").insertMany([
+        {
+          name: "Book 1",
+          year: 1995,
+          author: "Sofia",
+        },
+        {
+          name: "Book 2",
+          year: 2000,
+          author: "Nicolas",
+        },
+      ]);
+      console.log(seedData);
+
       // Act
       return request(app)
         .get("/api/v1/books")
@@ -45,7 +57,7 @@ describe("Test for books", () => {
         .then(({ body }) => {
           console.log(body);
           // Assert
-          expect(body.length).toEqual(fakeBooks.length);
+          expect(body.length).toEqual(seedData.insertedCount);
         });
     });
   });
